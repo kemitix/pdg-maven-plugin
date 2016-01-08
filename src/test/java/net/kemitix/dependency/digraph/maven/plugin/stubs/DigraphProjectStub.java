36d4340926c13ad0c8032ef35466c61406e3e1f9
Paul Campbell <paulcampbell@fife.ac.uk>
2016-01-08 12:08:25 +0000

    stubs.DigraphProjectStub: don't call overridable methods from constructor
    Move initialisation to init() method and have AbstractProjectsList call it.
    
    Signed-off-by: Paul Campbell <paulcampbell@fife.ac.uk>

--- src/test/java/net/kemitix/dependency/digraph/maven/plugin/stubs/DigraphProjectStub.java
+++ src/test/java/net/kemitix/dependency/digraph/maven/plugin/stubs/DigraphProjectStub.java
@@ -6,8 +6,10 @@ import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
 import org.codehaus.plexus.util.ReaderFactory;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,13 +30,19 @@ public class DigraphProjectStub extends MavenProjectStub {
      */
     public DigraphProjectStub(final String path) {
         basedir = new File(super.getBasedir() + path);
+    }
+
+    /**
+     * Initialise the stub.
+     */
+    public void init() {
         MavenXpp3Reader pomReader = new MavenXpp3Reader();
         Model model;
         try {
             model = pomReader.read(ReaderFactory.newXmlReader(
                     new File(getBasedir(), "pom.xml")));
             setModel(model);
-        } catch (Exception e) {
+        } catch (IOException | XmlPullParserException e) {
             throw new RuntimeException(e);
         }
 
