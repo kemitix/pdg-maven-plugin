68e9b2e657daeec63c6eb9eaea6a85741c995ad5
Paul Campbell <paulcampbell@fife.ac.uk>
2016-01-12 10:35:09 +0000

    NodeTreeDependencyData: added
    Signed-off-by: Paul Campbell <paulcampbell@fife.ac.uk>

---
+++ src/main/java/net/kemitix/dependency/digraph/maven/plugin/NodeTreeDependencyData.java
@@ -0,0 +1,71 @@
+package net.kemitix.dependency.digraph.maven.plugin;
+
+import net.kemitix.node.Node;
+import net.kemitix.node.NodeItem;
+import org.apache.maven.plugin.logging.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Implementation of {@link DependencyData} using a node tree.
+ *
+ * @author Paul Campbell
+ */
+public class NodeTreeDependencyData implements DependencyData {
+
+    private final Node<PackageData> root
+            = new NodeItem<>(new PackageData("[root]"));
+
+    @Override
+    public void addDependency(final String user, final String imported) {
+        final List<PackageData> userLine = createPackageLineList(user);
+        root.createDescendantLine(userLine);
+        final List<PackageData> importedLine
+                = createPackageLineList(imported);
+        root.createDescendantLine(importedLine);
+        root.walkTree(importedLine).ifPresent((Node<PackageData> i) -> {
+            root.walkTree(userLine).ifPresent((Node<PackageData> u) -> {
+                u.getData().getUses().add(i);
+            });
+        });
+    }
+
+    private List<PackageData> createPackageLineList(final String userPackage) {
+        List<PackageData> line = new ArrayList<>();
+        Arrays.asList(userPackage.split("\\."))
+                .forEach((String n) -> line.add(new PackageData(n)));
+        return line;
+    }
+
+    @Override
+    public void dumpDependencies(final Log log) {
+        log.info("Packages:");
+        logUsage(root);
+        root.getChildren().stream().map(this::logUsage).forEach(log::info);
+    }
+
+    private String logUsage(final Node<PackageData> node) {
+        final PackageData packageData = node.getData();
+        final String userName = packageData.getName();
+        final StringBuilder usages = new StringBuilder();
+        packageData.getUses()
+                .forEach((Node<PackageData> t) -> {
+                    usages.append("* ").append(userName).append(" => ")
+                            .append(t.getData().getName());
+                });
+        return usages.toString();
+    }
+
+    @Override
+    public List<String> getUserPackages() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<String> getUsedPackages(final String user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
