23ff0e1f6b2e916c5b40aaca44efe6e9137b7b53
Paul Campbell <paulcampbell@fife.ac.uk>
2016-01-12 10:31:46 +0000

    PackageData: added
    Signed-off-by: Paul Campbell <paulcampbell@fife.ac.uk>

---
+++ src/main/java/net/kemitix/dependency/digraph/maven/plugin/PackageData.java
@@ -0,0 +1,58 @@
+package net.kemitix.dependency.digraph.maven.plugin;
+
+import lombok.Getter;
+import net.kemitix.node.Node;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Defines a package.
+ *
+ * @author pcampbell
+ */
+public class PackageData {
+
+    @Getter
+    private final String name;
+
+    @Getter
+    private final Set<Node<PackageData>> uses = new HashSet<>();
+
+    /**
+     * Constructor.
+     *
+     * @param name the name of the package
+     */
+    public PackageData(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    @SuppressWarnings("magicnumber")
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PackageData other = (PackageData) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+}
