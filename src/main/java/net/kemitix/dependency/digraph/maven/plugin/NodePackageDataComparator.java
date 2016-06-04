9711e6c084c75aea79699c638d7da4575d8ccf7e
Paul Campbell <pcampbell@kemitix.net>
2016-06-04 18:58:15 +0100

    NodePackageDataComparator: use NodeHelper.getRequiredData()

--- src/main/java/net/kemitix/dependency/digraph/maven/plugin/NodePackageDataComparator.java
+++ src/main/java/net/kemitix/dependency/digraph/maven/plugin/NodePackageDataComparator.java
@@ -1,7 +1,5 @@
 package net.kemitix.dependency.digraph.maven.plugin;
 
-import lombok.val;
-
 import java.io.Serializable;
 import java.util.Comparator;
 
@@ -20,13 +18,8 @@ class NodePackageDataComparator
     @Override
     public int compare(
             final Node<PackageData> o1, final Node<PackageData> o2) {
-        val pd1 = o1.getData();
-        val pd2 = o2.getData();
-        if (pd1.isPresent() && pd2.isPresent()) {
-            val n1 = pd1.get().getName();
-            val n2 = pd2.get().getName();
-            return n1.compareTo(n2);
-        }
-        throw new IllegalStateException("Node has no package data");
+        return NodeHelper.getRequiredData(o1)
+                         .getName()
+                         .compareTo(NodeHelper.getRequiredData(o2).getName());
     }
 }
