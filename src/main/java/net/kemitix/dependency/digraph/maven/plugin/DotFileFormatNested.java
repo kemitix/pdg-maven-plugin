847d2f028b67a0e1edbee931ff23911c40051e9b
Paul Campbell <pcampbell@kemitix.net>
2016-08-18 15:14:18 +0100

    DotFileFormatNested: comply with CyclomaticComplexity

--- src/main/java/net/kemitix/dependency/digraph/maven/plugin/DotFileFormatNested.java
+++ src/main/java/net/kemitix/dependency/digraph/maven/plugin/DotFileFormatNested.java
@@ -1,12 +1,12 @@
 package net.kemitix.dependency.digraph.maven.plugin;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import net.kemitix.dependency.digraph.maven.plugin.digraph.EdgeElement;
 import net.kemitix.dependency.digraph.maven.plugin.digraph.Subgraph;
 import net.kemitix.node.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Generates a dot file dependency report as nested clusters.
@@ -21,12 +21,41 @@ class DotFileFormatNested extends AbstractDotFileFormat {
         super(base, nodePathGenerator);
     }
 
+    private void addAnyLHead(final Node<PackageData> headNode,
+            final Node<PackageData> tailNode, final List<String> attributes) {
+        // if head node has children, then add lhead attribute
+        if (headNode.getChildren().size() > 0 && !tailNode.isDescendantOf(
+                headNode)) {
+            attributes.add(String.format("lhead=\"cluster%s\"",
+                    getClusterId(headNode)));
+        }
+    }
+
+    private void addAnyLTail(final Node<PackageData> tailNode,
+            final Node<PackageData> headNode, final List<String> attributes) {
+        // if tail node has children, then add ltail attribute
+        if (tailNode.getChildren().size() > 0 && !headNode.isDescendantOf(
+                tailNode)) {
+            attributes.add(String.format("ltail=\"cluster%s\"",
+                    getClusterId(tailNode)));
+        }
+    }
+
+    private String buildAttributeTag(final List<String> attributes) {
+        String attributeTag = "";
+        if (attributes.size() > 0) {
+            attributeTag = String.format("[%s]",
+                    attributes.stream().collect(Collectors.joining(",")));
+        }
+        return attributeTag;
+    }
+
     @Override
     String render(final Subgraph subgraph) {
         final String label = quoted(subgraph.getLabel());
         final String id = quoted(subgraph.getId());
         return String.format("subgraph %s{%n" + "label=%s%n"
-                        + "%s[label=\"\",style=\"invis\",width=0]%n" + "%s%n}",
+                + "%s[label=\"\",style=\"invis\",width=0]%n" + "%s%n}",
                 quoted("cluster" + subgraph.getId()), label, id,
                 renderElements(subgraph.getElements()));
     }
@@ -35,30 +64,13 @@ class DotFileFormatNested extends AbstractDotFileFormat {
     String render(
             final EdgeElement edgeElement) {
         List<String> attributes = new ArrayList<>();
-
         final Node<PackageData> tailNode = edgeElement.getTail()
-                                                      .getPackageDataNode();
+                .getPackageDataNode();
         final Node<PackageData> headNode = edgeElement.getHead()
-                                                      .getPackageDataNode();
-
-        // if tail node has children, then add ltail attribute
-        if (tailNode.getChildren().size() > 0 && !headNode.isDescendantOf(
-                tailNode)) {
-            attributes.add(String.format("ltail=\"cluster%s\"",
-                    getClusterId(tailNode)));
-        }
-        // if head node has children, then add lhead attribute
-        if (headNode.getChildren().size() > 0 && !tailNode.isDescendantOf(
-                headNode)) {
-            attributes.add(String.format("lhead=\"cluster%s\"",
-                    getClusterId(headNode)));
-        }
-        String attributeTag = "";
-        if (attributes.size() > 0) {
-            attributeTag = String.format("[%s]",
-                    attributes.stream().collect(Collectors.joining(",")));
-        }
-
+                .getPackageDataNode();
+        addAnyLTail(tailNode, headNode, attributes);
+        addAnyLHead(headNode, tailNode, attributes);
+        String attributeTag = buildAttributeTag(attributes);
         return String.format("%s->%s%s", quoted(getNodeId(tailNode)),
                 quoted(getNodeId(headNode)), attributeTag);
     }
