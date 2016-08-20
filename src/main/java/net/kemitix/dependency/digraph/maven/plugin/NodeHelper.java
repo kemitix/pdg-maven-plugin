d4f42645f941897f2a2487e69ef0df96cdba8bd8
Paul Campbell <pcampbell@kemitix.net>
2016-08-20 19:30:20 +0100

    NodeHelper: add copyOf(Node<PackageData>)

--- src/main/java/net/kemitix/dependency/digraph/maven/plugin/NodeHelper.java
+++ src/main/java/net/kemitix/dependency/digraph/maven/plugin/NodeHelper.java
@@ -1,13 +1,16 @@
 package net.kemitix.dependency.digraph.maven.plugin;
 
+import lombok.val;
+
 import net.kemitix.node.Node;
+import net.kemitix.node.Nodes;
 
 /**
  * Helpers for Node.
  *
  * @author pcampbell
  */
-final class NodeHelper {
+public final class NodeHelper {
 
     private NodeHelper() {
     }
@@ -17,6 +20,30 @@ final class NodeHelper {
         return node.getData()
                    .orElseThrow(() -> new IllegalStateException(
                            "Node has no package data"));
+    }
+
+    /**
+     * Creates a copy of the package data node and returns it.
+     *
+     * @param source the node to copy
+     *
+     * @return a copy of the package data node
+     */
+    public static Node<PackageData> copyOf(final Node<PackageData> source) {
+        val optionalData = source.getData();
+        PackageData data = null;
+        if (optionalData.isPresent()) {
+            data = optionalData.get();
+        }
+        val optionalParent = source.getParent();
+        Node<PackageData> parent = null;
+        if (optionalParent.isPresent()) {
+            parent = optionalParent.get();
+        }
+        if (parent != null) {
+            return Nodes.namedChild(data, source.getName(), parent);
+        }
+        return Nodes.namedRoot(data, source.getName());
     }
 
 }
