40e8477df1ff077d03e04a11c1a489221252d2a2
Paul Campbell <pcampbell@kemitix.net>
2019-01-06 18:41:41 +0000

    Update changelog, readme, jenkins and pom (#10)
    * [pom] update git links
    
    * [changelog] convert to org-mode format
    
    * [readme] adjust heading levels
    
    * [jenkins] update and remove sonarcloud and codacy
    
    * [pom] lower coverage requirements

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,5 @@
 final String publicRepo = 'https://github.com/kemitix/'
 final String mvn = "mvn --batch-mode --update-snapshots --errors"
-final String repo = "kemitix/pdg-maven-plugin"
 
 pipeline {
     agent any
@@ -8,64 +7,23 @@ pipeline {
         stage('Build & Test') {
             steps {
                 withMaven(maven: 'maven', jdk: 'JDK 1.8') {
-                    sh "${mvn} clean test"
-                }
-            }
-        }
-        stage('Quality Checks (PMD & Checkstyle)') {
-            steps {
-                withMaven(maven: 'maven', jdk: 'JDK 1.8') {
-                    sh "${mvn} checkstyle:checkstyle pmd:pmd"
+                    sh "${mvn} clean compile checkstyle:checkstyle pmd:pmd test"
+                    // Code Coverage to Jenkins
+                    jacoco exclusionPattern: '**/*{Test|IT|Main|Application|Immutable}.class'
+                    // PMD to Jenkins
                     pmd canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '', unHealthy: ''
-                    checkstyle defaultEncoding: 'UTF-8', healthy: '', pattern: '', shouldDetectModules: true, unHealthy: '', useDeltaValues: true, usePreviousBuildAsReference: true, useStableBuildAsReference: true
+                    // Checkstyle to Jenkins
+                    step([$class: 'hudson.plugins.checkstyle.CheckStylePublisher',
+                          pattern: '**/target/checkstyle-result.xml',
+                          healthy:'20',
+                          unHealthy:'100'])
                 }
             }
         }
-        stage('Record Coverage (all)') {
-            steps {
-                // jacoco exclusionPattern: '**/*{Test|IT|Main|Application|Immutable}.class'
-                withMaven(maven: 'maven', jdk: 'JDK 1.8') {
-                    // Codacy
-                    sh "${mvn} jacoco:report com.gavinmogan:codacy-maven-plugin:coverage " +
-                        "-DcoverageReportFile=target/site/jacoco/jacoco.xml " +
-                        "-DprojectToken=`$JENKINS_HOME/codacy/token` " +
-                        "-DapiToken=`$JENKINS_HOME/codacy/apitoken` " +
-                        "-Dcommit=`git rev-parse HEAD`"
-                }
-            }
-        }
-        stage('Record Coverage (master)') {
-            when { branch 'master' }
-            steps {
-                script {
-                    currentBuild.result = 'SUCCESS'
-                }
-                step([$class: 'MasterCoverageAction', scmVars: [GIT_URL: env.GIT_URL]])
-            }
-        }
-        stage('Coverage to Github (Pull Request)') {
-            when { allOf {not { branch 'master' }; expression { return env.CHANGE_ID != null }} }
-            steps {
-                script {
-                    currentBuild.result = 'SUCCESS'
-                }
-                step([$class: 'CompareCoverageAction', scmVars: [GIT_URL: env.GIT_URL]])
-            }
-        }
-        stage('Verify') {
+        stage('Verify & Install') {
             steps {
                 withMaven(maven: 'maven', jdk: 'JDK 1.8') {
-                    sh "${mvn} --activate-profiles verify verify"
-                }
-            }
-        }
-        stage('SonarQube (published)') {
-            when { expression { isPublished(publicRepo) } }
-            steps {
-                withSonarQubeEnv('sonarqube') {
-                    withMaven(maven: 'maven', jdk: 'JDK 1.8') {
-                        sh "${mvn} org.sonarsource.scanner.maven:sonar-maven-plugin:3.4.0.905:sonar -Dsonar.pullrequst.base=master -Dsonar.pullrequest.branch=${env.GIT_BRANCH} -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.key=${env.GIT_BRANCH} -Dsonar.pullrequest.repository=${repo}"
-                    }
+                    sh "${mvn} -DskipTests install"
                 }
             }
         }
